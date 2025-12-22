60d1597d68b1e5e548524983dc3c14046d3a082c
cyc9805 <cyc9805@gmail.com>
2025-12-22 09:27:33 +0000

    fix: served_model.sh

--- serve_model.sh
+++ serve_model.sh
@@ -3,8 +3,9 @@ export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export MPLBACKEND=Agg
 export PYTHONWARNINGS="ignore:findfont"
 export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+export CHECKPOINT_PATH="[path/to/your/checkpoint]"
 
-vllm serve /mnt/nlpai-storage/eval_team/hf_ckpt/SFT/ckpt/stg3-1120-SFT-train_multiturn_251127_gbs-512_lr-1e-5_minlr-4e-6/iter_0002190/hf \
+vllm serve $CHECKPOINT_PATH \
     --dtype auto \
     --served-model-name VAETKI \
     --enable-auto-tool-choice \
